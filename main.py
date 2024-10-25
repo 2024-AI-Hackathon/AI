188fcedc522db5b2a51670c123669453b1d1bd84
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-26 06:09:21 +0900

    tts 웹소켓 방식으로 변경

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI
-from stt import get_audio
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
 from tts import speak
 
 app = FastAPI()
 
-# 엔드포인트를 통해서만 get_audio가 호출되도록 설정
-@app.get("/stt/")
-async def speech_to_text():
-    return get_audio()
+# 정적 파일 제공 설정 (HTTP 요청 처리)
+app.mount("/static", StaticFiles(directory=".", html=True), name="static")
 
-@app.get("/tts/")
-async def text_to_speech(text: str):
-    return speak(text, save_path="voice.mp3")
-    
+
+
+
+
+@app.websocket("/ws/tts")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received: {data}")
+            speak(data, save_path="voice.mp3")
+            await websocket.send_text(f"Received and spoken: {data}")
+    except WebSocketDisconnect:
+        print("WebSocket connection closed")
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
