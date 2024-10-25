8b6758a2e690a98e37b57c8963c5f92a98e5b013
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-26 04:19:46 +0900

    tts 기본 기능 구현

--- hackaton/bin/activate.fish
+++ hackaton/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hellosj00/dev/AI/hackaton"
+set -gx VIRTUAL_ENV "/Users/kimminsang/Desktop/AI/hackaton"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
